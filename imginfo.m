3fadd0e274698af31ab0be98018fa3e3cfabad4d
noelpei0461 <31324311+noelpei0461@users.noreply.github.com>
2017-09-17 04:48:38 +0800

    Add files via upload

--- imginfo.m
+++ imginfo.m
@@ -1,8 +1,11 @@
 function im=imginfo(I)
-x=imfinfo(I)
-a=x.BitDepth
-b=x.FileSize
-c=x.FileModDate
-cc=struct('bitdepth',a,'size',b,'date',c)
-im=cc
+x=imfinfo(I);
+a=x.BitDepth;
+b=x.FileSize;
+c=x.FileModDate;
+d1=strfind(x.ImageDescription,'Date=')+5;
+d2=strfind(x.ImageDescription,'Time=')-3;
+dx=x.ImageDescription(d1:d2);
+cc=struct('bitdepth',a,'size',b,'date',dx);
+im=cc;
 end
